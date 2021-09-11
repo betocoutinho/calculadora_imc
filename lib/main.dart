a4a07c8a00f8427e009f2dc5aabf61d881609e0d
Roberto Coutinho <64106750+betocoutinho@users.noreply.github.com>
2021-09-11 18:39:44 -0300

    Aplicação do scrollview e formatações do botão.

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-void main(){
-  runApp(MaterialApp(title: "Calculadora IMC",
-    home: Home(),));
+
+void main() {
+  runApp(MaterialApp(
+    title: "Calculadora IMC",
+    home: Home(),
+  ));
 }
 
 class Home extends StatefulWidget {
@@ -16,39 +19,57 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Calculadora IMC"),
-        centerTitle: true,
-        backgroundColor: Colors.blueGrey,
-        actions: [
-          IconButton(onPressed: (){}, icon: Icon(Icons.refresh))
-        ],
-      ),
-      backgroundColor: Colors.white,
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: const [
-          Icon(Icons.person_outline, size: 120.0, color: Colors.blueGrey),
-          TextField(
-            keyboardType: TextInputType.number,
-            decoration: InputDecoration(
-              labelText: "Peso (KG)",
-              labelStyle: TextStyle(color: Colors.blueGrey)
-            ),
-            textAlign: TextAlign.center,
-            style: TextStyle(color: Colors.blueGrey, fontSize: 25.0),
+        appBar: AppBar(
+          title: Text("Calculadora IMC"),
+          centerTitle: true,
+          backgroundColor: Colors.blueGrey,
+          actions: [IconButton(onPressed: () {}, icon: Icon(Icons.refresh))],
+        ),
+        backgroundColor: Colors.white,
+        body: SingleChildScrollView(
+          padding: EdgeInsets.fromLTRB(10.0, 0.0, 10.0, 0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Icon(Icons.person_outline, size: 120.0, color: Colors.blueGrey),
+              TextField(
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                    labelText: "Peso (KG)",
+                    labelStyle: TextStyle(color: Colors.blueGrey)),
+                textAlign: TextAlign.center,
+                style: TextStyle(color: Colors.blueGrey, fontSize: 25.0),
+              ),
+              TextField(
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                    labelText: "Altura (cm)",
+                    labelStyle: TextStyle(color: Colors.blueGrey)),
+                textAlign: TextAlign.center,
+                style: TextStyle(color: Colors.blueGrey, fontSize: 25.0),
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 10.0, bottom: 10.0),
+                child: Container(
+                  height: 50.0,
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    child: Text("Calcular IMC"),
+                    style: ButtonStyle(
+                        backgroundColor:
+                            MaterialStateProperty.all<Color>(Colors.blueGrey),
+                        textStyle: MaterialStateProperty.all<TextStyle>(
+                            TextStyle(color: Colors.white, fontSize: 25.0))),
+                  ),
+                ),
+              ),
+              Text(
+                "info",
+                style: TextStyle(color: Colors.blueGrey, fontSize: 25.0),
+                textAlign: TextAlign.center,
+              )
+            ],
           ),
-          TextField(
-            keyboardType: TextInputType.number,
-            decoration: InputDecoration(
-                labelText: "Altura (cm)",
-                labelStyle: TextStyle(color: Colors.blueGrey)
-            ),
-            textAlign: TextAlign.center,
-            style: TextStyle(color: Colors.blueGrey, fontSize: 25.0),
-          )
-        ],
-      ),
-    );
+        ));
   }
 }
